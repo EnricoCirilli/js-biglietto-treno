c236b1849a7f16c8a4b80432a8de1ae7257819ab
EnricoCirilli <enrico.cirilli@gmail.com>
2023-10-18 21:30:10 +0200

    add: debugging

--- js/script.js
+++ js/script.js
@@ -1,34 +1,35 @@
+// chiedo all'utente quanti km e quanti anni ha
 const userKm = prompt("Ciao Utente! Quanti Km vuoi percorrere?");
 const userAge = prompt("Ciao Utente! Quanti Anni hai?");
-console.log(userAge, userKm);
-
+console.log(userKm, userAge);
+// calcolo il costo per km
 const costoKm = (userKm * 0.21);
 console.log(costoKm);
-
+//calcolo il primo sconto da sottrarre successivamente al costo del biglietto
 let discountFisrt = (userAge * 0.20);
 console.log(discountFisrt);
-
+//calcolo il secondo sconto da sottrarre successivamente al costo del biglietto
 let discountSecond = (userAge * 0.40);
 console.log(discountSecond);
-
+// il prezzo con lo sconto del 20%
 let priceFirst = (costoKm - discountFisrt);
 console.log(priceFirst);
-
+// il prezzo del biglietto con lo sconto del 40%
 let priceSecond = (costoKm - discountSecond);
 console.log(priceSecond);
 
-let totalPriceFirst = (costoKm - priceFirst);
-console.log(totalPriceFirst);
-
-let totalPriceSecond = (costoKm - priceSecond);
-console.log(totalPriceSecond);
 
 
-if (userAge > 65) {
-    console.log(Math.round(totalPriceSecond));
-} else if (userAge < 18) {
-    console.log(Math.round(totalPriceFirst));
+if (userAge < 18) {
+    console.log(Math.round(priceFirst));
+} else if (userAge > 65) {
+    console.log(Math.round(priceSecond));
 } else console.log(Math.round(costoKm));
+
+
+document.getElementById("message").innerHTML= 'euro';
+
+
 
 /*if ( userAge > 65 ) {
     console.log(Math.round(userAge * 0.40) - costoKm);
