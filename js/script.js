747894ac75b70c5a3d1a933f521562c1288682f1
EnricoCirilli <enrico.cirilli@gmail.com>
2023-10-18 18:25:52 +0200

    add: exercise loading

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
-alert('hello world')
+const userKm = prompt("Ciao Utente! Quanti Km vuoi percorrere?");
+const userAge = prompt("Ciao Utente! Quanti Anni hai?");
+console.log(userAge, userKm);
+
+const costoKm = (userKm * 0.21);
+console.log(costoKm);
+
+let discountFisrt = (userAge * 0.20);
+console.log(discountFisrt);
+
+let discountSecond = (userAge * 0.40);
+console.log(discountSecond);
+
+let priceFirst = (costoKm - discountFisrt);
+console.log(priceFirst);
+
+let priceSecond = (costoKm - discountSecond);
+console.log(priceSecond);
+
+let totalPriceFirst = (costoKm - priceFirst);
+console.log(totalPriceFirst);
+
+let totalPriceSecond = (costoKm - priceSecond);
+console.log(totalPriceSecond);
+
+let totalPrice = '';
+
+if (userAge > 65) {
+    console.log(Math.round(totalPriceSecond));
+} else if (userAge < 18) {
+    console.log(Math.round(totalPriceFirst));
+} else console.log(Math.round(costoKm));
+
+/*if ( userAge > 65 ) {
+    console.log(Math.round(userAge * 0.40) - costoKm);
+}else if (userAge < 18) {
+    console.log(Math.round(userAge * 0.20) - costoKm);
+}else console.log(Math.round(costoKm));*/
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -1,7 +1,29 @@
-#### Raccolta Dati
+
+##Esercizio
+
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+
+## Soluzione
+##### Dati:
+1. chiedere all'utente numero km che vuole percorrere.
+2. chiedere all'utente la sua età.
+
+
 
 
 #### Logica del Programma
+3. formula per calcolare il costo in km senza sconto.
+4. formula per calcolare lo sconto del 20%
+5. formula per calcolare lo sconto del 40%
+6. formula per calcolare il prezzo totale del primo sconto
+7. formula per calcolare il prezzo totale del socndo sconto.
+8. applico formula condizionale per far si che si applichi una condizione in una delle tre situazioni.
 
 
 #### Ouput del Messaggio
